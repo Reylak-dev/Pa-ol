b4679d2b751e1c25f2d08bf5e13b3cb073439b43
Reylak <reylak@disroot.org>
2026-05-09 10:36:59 -0300

    codigo refinado

--- pañol.py
+++ pañol.py
@@ -2,7 +2,7 @@ import sqlite3
 
 class pañol:
     def __init__(self):      
-        self.conexion = sqlite3.connect("pañol8.db")
+        self.conexion = sqlite3.connect("pañol.db")
         self.cursor = self.conexion.cursor()
         self.iniciarTabla()
 
@@ -10,9 +10,7 @@ class pañol:
         self.cursor.execute("CREATE TABLE IF NOT EXISTS herramientas (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT,canti INTEGER)") 
         self.conexion.commit()
 
-    def agregarProducto(self):
-        nombre = input("Ingresa el nombre: ")
-        canti = int(input("Ingresa la cantidad: "))
+    def agregarProducto(self, nombre: str, canti: int):
         self.cursor.execute("INSERT INTO herramientas (nombre, canti) VALUES (?,?)", (nombre, canti))
         self.conexion.commit()
 
@@ -22,15 +20,11 @@ class pañol:
         for producto in productos:
             print(producto)
     
-    def eliminarProducto(self):
-        id = input("Ingresa el id del producto a eliminar: ")
-        self.cursor.execute("DELETE FROM herramientas WHERE id = ?", (id))
+    def eliminarProducto(self, id: int):
+        self.cursor.execute("DELETE FROM herramientas WHERE id = ?", (id,))
         self.conexion.commit()
 
-    def editarProducto(self):
-        nombre = input("nombre: ")
-        cantidad = int(input("cantidad: "))
-        id = int(input("id: "))
+    def editarProducto(self, nombre: str, cantidad: int, id: int):
         self.cursor.execute("UPDATE herramientas SET nombre = ?, canti = ? WHERE id = ?", (nombre, cantidad, id))
         self.conexion.commit()
 
@@ -38,19 +32,24 @@ Pañol = pañol()
 
 
 while True:
-    print("1: agregar producto, 2: ver herramientas, 3: eliminar productos, 4: editar productos, 5: salir")
-    opcion = input("Que queres hacer?: ")
+    print("Pañol de la técnica")
+    print()
+    print("[1]: Agregar herramienta")
+    print("[2]: Ver herramientas")
+    print("[3]: Eliminar herramienta")
+    print("[4]: Editar herramienta")
+    print("[5]: Salir de la DB")
+    print()
+    opcion = int(input("Ingrese la acción a realizar: "))
 
 
-    if opcion == "1":
-        Pañol.agregarProducto()
-    elif opcion == "2":
+    if opcion == 1:
+        Pañol.agregarProducto(input("Nombre: "), int(input("Cantidad: ")))
+    elif opcion == 2:
         Pañol.verProductos()
-    elif opcion == "3":
-        Pañol.eliminarProducto()
-    elif opcion == "4":
-        Pañol.editarProducto()
-    elif opcion == "5":
+    elif opcion == 3:
+        Pañol.eliminarProducto(int(input("ID: ")))
+    elif opcion == 4:
+        Pañol.editarProducto(input("Nombre: "), int(input("Cantidad: ")), int(input("ID: ")))
+    elif opcion == 5:
         break
-
-    
